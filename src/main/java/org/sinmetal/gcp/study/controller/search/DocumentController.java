26e0300330699445dc9f101ec293b81992ba95ee
Shingo Ishimura <metal.tie@gmail.com>
2014-02-28 18:14:39 +0900

    GET処理を作成した

--- src/main/java/org/sinmetal/gcp/study/controller/search/DocumentController.java
+++ src/main/java/org/sinmetal/gcp/study/controller/search/DocumentController.java
@@ -1,23 +1,18 @@
 package org.sinmetal.gcp.study.controller.search;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
+import org.sinmetal.gcp.study.service.SearchService;
+import org.sinmetal.gcp.study.service.SearchService.Type;
 import org.sinmetal.gcp.study.util.JsonUtil;
 import org.sinmetal.gcp.study.util.RequestUtil;
 import org.slim3.controller.Navigation;
 import org.slim3.controller.SimpleController;
 
 import com.google.appengine.api.search.Document;
-import com.google.appengine.api.search.Document.Builder;
-import com.google.appengine.api.search.Field;
-import com.google.appengine.api.search.Index;
-import com.google.appengine.api.search.IndexSpec;
+import com.google.appengine.api.search.GetResponse;
 import com.google.appengine.api.search.PutResponse;
-import com.google.appengine.api.search.SearchServiceFactory;
 
 /**
  * Search API Sample
@@ -29,7 +24,8 @@ public class DocumentController extends SimpleController {
 	@Override
 	protected Navigation run() throws Exception {
 		if (isGet()) {
-			throw new UnsupportedOperationException("unknown method =" + request.getMethod());
+			doGet();
+			return null;
 		}
 		if (isPost()) {
 			doPost();
@@ -42,59 +38,22 @@ public class DocumentController extends SimpleController {
 		throw new UnsupportedOperationException("unknown method =" + request.getMethod());
 	}
 
+	void doGet() throws Exception {
+		GetResponse<Document> documents = SearchService.getAll();
+
+		response.setHeader("Content-Type", "application/json");
+		response.getWriter().write(JsonUtil.encode(documents));
+		response.getWriter().flush();
+	}
+
 	void doPost() throws Exception {
 		final String requestBody = RequestUtil.getBody(request);
 		PostDataForSearchDocument postData =
 				JsonUtil.decode(requestBody, PostDataForSearchDocument.class);
-		PutResponse putResponse = addToIndex(postData);
+		PutResponse putResponse = SearchService.addToIndex(postData);
 		response.setHeader("Content-Type", "application/json");
 		response.getWriter().write(JsonUtil.encode(putResponse));
 		response.getWriter().flush();
-	}
-
-	static PutResponse addToIndex(PostDataForSearchDocument postData) throws ParseException {
-		// @formatter:off
-		Builder builder = Document.newBuilder()
-			.setId(postData.id)
-			.setLocale(Locale.JAPAN);
-		// @formatter:on
-		for (IndexParamForSearchDocument param : postData.indexParams) {
-			switch (param.type) {
-				case Atom:
-					builder.addField(Field.newBuilder().setName(param.name).setAtom(param.value));
-					break;
-				case Text:
-					builder.addField(Field.newBuilder().setName(param.name).setText(param.value));
-					break;
-				case HTML:
-					builder.addField(Field.newBuilder().setName(param.name).setHTML(param.value));
-					break;
-				case Number:
-					builder.addField(Field.newBuilder().setName(param.name)
-						.setNumber(Double.parseDouble(param.value)));
-					break;
-				case Date:
-					SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
-					builder.addField(Field.newBuilder().setName(param.name)
-						.setDate(dateFormat.parse(param.value)));
-					break;
-				case Geopoint:
-					// TODO Geopoint
-					break;
-				default:
-					throw new IllegalArgumentException("unknown type = " + param.type);
-			}
-		}
-		return getIndex().put(builder.build());
-	}
-
-	/**
-	 * @return {@link Index} for document of Sample.
-	 * @author sinmetal
-	 */
-	public static Index getIndex() {
-		return SearchServiceFactory.getSearchService().getIndex(
-				IndexSpec.newBuilder().setName("Sample").build());
 	}
 
 
@@ -118,12 +77,5 @@ public class DocumentController extends SimpleController {
 		public Type type;
 
 		public String value;
-	}
-
-	/**
-	 * @author sinmetal
-	 */
-	public enum Type {
-		Atom, Text, HTML, Number, Date, Geopoint
 	}
 }
--- src/test/java/org/sinmetal/gcp/study/controller/search/DocumentControllerTest.java
+++ src/test/java/org/sinmetal/gcp/study/controller/search/DocumentControllerTest.java
@@ -1,15 +1,13 @@
 package org.sinmetal.gcp.study.controller.search;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
 import org.sinmetal.gcp.study.controller.ApiTestCase;
 import org.sinmetal.gcp.study.controller.search.DocumentController.IndexParamForSearchDocument;
 import org.sinmetal.gcp.study.controller.search.DocumentController.PostDataForSearchDocument;
-import org.sinmetal.gcp.study.controller.search.DocumentController.Type;
+import org.sinmetal.gcp.study.service.SearchService;
+import org.sinmetal.gcp.study.service.SearchService.Type;
 import org.sinmetal.gcp.study.test.AssertionUtil;
 import org.sinmetal.gcp.study.util.JsonUtil;
 
@@ -25,6 +23,29 @@ import static org.junit.Assert.*;
  * @author sinmetal
  */
 public class DocumentControllerTest extends ApiTestCase {
+
+	/**
+	 * @throws Exception
+	 * @author sinmetal
+	 */
+	@Test
+	public void testGet() throws Exception {
+		PostDataForSearchDocument postData = new PostDataForSearchDocument();
+		postData.id = "hogeId";
+		{
+			IndexParamForSearchDocument param = new IndexParamForSearchDocument();
+			param.name = "name";
+			param.value = "hoge";
+			param.type = Type.Atom;
+			postData.indexParams.add(param);
+		}
+		SearchService.addToIndex(postData);
+
+		tester.request.setMethod("GET");
+		tester.start("/search/document");
+		assertThat(tester.getController(), instanceOf(DocumentController.class));
+		assertThat(tester.response.getStatus(), is(200));
+	}
 
 	/**
 	 * @throws Exception
@@ -52,43 +73,10 @@ public class DocumentControllerTest extends ApiTestCase {
 		assertThat(tester.response.getOutputAsString(),
 				is("{\"results\":[{\"code\":\"OK\",\"message\":null}],\"ids\":[\"hogeId\"]}"));
 
-		Document document = DocumentController.getIndex().get(postData.id);
+		Document document = SearchService.getIndex().get(postData.id);
 		assertThat(document, notNullValue());
 
-		List<Field> fields = buildFields(postData.indexParams);
+		List<Field> fields = SearchService.buildFields(postData.indexParams);
 		AssertionUtil.assertFtsDocument(document, fields);
-	}
-
-	List<Field> buildFields(List<IndexParamForSearchDocument> data) throws ParseException {
-		List<Field> fields = new ArrayList<>();
-		for (IndexParamForSearchDocument param : data) {
-			Field.newBuilder().setName(param.name);
-			switch (param.type) {
-				case Atom:
-					fields.add(Field.newBuilder().setName(param.name).setAtom(param.value).build());
-					break;
-				case Text:
-					fields.add(Field.newBuilder().setName(param.name).setText(param.value).build());
-					break;
-				case HTML:
-					fields.add(Field.newBuilder().setName(param.name).setHTML(param.value).build());
-					break;
-				case Number:
-					fields.add(Field.newBuilder().setName(param.name)
-						.setNumber(Double.parseDouble(param.value)).build());
-					break;
-				case Date:
-					SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
-					fields.add(Field.newBuilder().setName(param.name)
-						.setDate(dateFormat.parse(param.value)).build());
-					break;
-				case Geopoint:
-					// TODO Geopoint
-					break;
-				default:
-					throw new IllegalArgumentException("unknown type = " + param.type);
-			}
-		}
-		return fields;
 	}
 }
