a9179c235948ffeb579c9103b42495cdae8ca22f
Shingo Ishimura <metal.tie@gmail.com>
2014-05-04 15:39:32 +0900

    update

--- tools/deploy/deploy.go
+++ tools/deploy/deploy.go
@@ -17,7 +17,8 @@ func main() {
 	fmt.Println("deploy is", os.Getenv("deploy"))
 
 	cmd := exec.Command(
-	"appcfg.sh",
+		//"appcfg.sh", "--version=gcps0426a", "--application=sandbox4sinmetal-tg","update" ,"~/workspace/java20131116/gcp-study/src/main/webapp",
+		"appcfg.sh", "--oauth2", "--version=gcps0426a", "--application=sandbox4sinmetal-tg", "update" ,"/Users/shingoishimura/workspace/java20131116/gcp-study/src/main/webapp",
 		//"deploy",
 		//"/Users/shingoishimura/bin/appengine-java-sdk-1.8.9/bin/appcfg.sh",
 		//"--oauth2",
@@ -37,18 +38,40 @@ func main() {
 //	cmd.Path = "appcfg.sh"
 //	cmd.Args[0] = "appcfg.sh"
 
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("cmd.Start start!!")
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("cmd.Start end!!")
+
+
+
+	fmt.Printf("cmd.Wait start!!")
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("cmd.Wait end!!")
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(stdout)
+	fmt.Printf("%s", buf.String())
+
 	cmd.Stdin = strings.NewReader("some input")
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
 	cmd.Stderr = &stderr
-	err := cmd.Run()
+	//err := cmd.Run()
 	if err != nil {
-		fmt.Printf("せつない")
-		fmt.Printf("stderr: %q\n", stderr.String())
+		fmt.Printf("stdout: %s\n", out.String())
+		fmt.Printf("stderr: %s\n", stderr.String())
 		log.Fatal(err)
 	}
-	fmt.Printf("stdout: %q\n", out.String())
+	fmt.Printf("stdout: %s\n", out.String())
 
 
 
