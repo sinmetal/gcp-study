b810e3d9c4c921a97f4b9e1b1abe58ff1068a6de
Shingo Ishimura <metal.tie@gmail.com>
2014-05-06 18:42:34 +0900

    stdin,stdout,stderrをシンプルに入れれば良いだけだった

--- tools/deploy/deploy.go
+++ tools/deploy/deploy.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	//"bytes"
-	"log"
-	"io"
-	"os"
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
-	//"strings"
 )
 
 func main() {
@@ -18,86 +15,18 @@ func main() {
 	fmt.Println("deploy is", os.Getenv("deploy"))
 
 	cmd := exec.Command(
-		//"appcfg.sh", "--version=gcps0426a", "--application=sandbox4sinmetal-tg","update" ,"~/workspace/java20131116/gcp-study/src/main/webapp",
-		"appcfg.sh", "--oauth2", "--version=gcps0426a", "--application=sandbox4sinmetal-tg", "update" ,"/Users/shingoishimura/workspace/java20131116/gcp-study/src/main/webapp",
-		//"deploy",
-		//"/Users/shingoishimura/bin/appengine-java-sdk-1.8.9/bin/appcfg.sh",
-		//"--oauth2",
-		//"--version=gcps0407a",
-		//"--application=sandbox4sinmetal-tg",
-		//"update",
-		//"~/workspace/java20131116/gcp-study/src/main/webapp")
+		"appcfg.sh", "--version=gcps0426c", "--application=map-sample-201300606", "--passin", "--no_cookies", "update", "/Users/shingoishimura/workspace/java20131116/gcp-study/src/main/webapp",
 	)
 
-//	cmd := exec.Cmd{
-//		Path: "appcfg.sh",
-//		Args: []string{"appcfg.sh"},
-//		Dir: "/Users/shingoishimura/bin/appengine-java-sdk-1.8.9/bin/",
-//	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
-//	cmd.Dir = "/Users/shingoishimura/bin/appengine-java-sdk-1.8.9/bin/"
-//	cmd.Path = "appcfg.sh"
-//	cmd.Args[0] = "appcfg.sh"
-
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("cmd.Start start!!")
 	if err := cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("cmd.Start end!!")
-
-	go io.Copy(os.Stdout, stdout)
 
 	if err := cmd.Wait(); err != nil {
 		log.Fatal(err)
 	}
-
-//	fmt.Printf("cmd.Wait start!!")
-//	if err := cmd.Wait(); err != nil {
-//		log.Fatal(err)
-//	}
-//	fmt.Printf("cmd.Wait end!!")
-//
-//	buf := new(bytes.Buffer)
-//	buf.ReadFrom(stdout)
-//	fmt.Printf("%s", buf.String())
-//
-//	cmd.Stdin = strings.NewReader("some input")
-//	var out bytes.Buffer
-//	var stderr bytes.Buffer
-//	cmd.Stdout = &out
-//	cmd.Stderr = &stderr
-//	//err := cmd.Run()
-//	if err != nil {
-//		fmt.Printf("stdout: %s\n", out.String())
-//		fmt.Printf("stderr: %s\n", stderr.String())
-//		log.Fatal(err)
-//	}
-//	fmt.Printf("stdout: %s\n", out.String())
-
-//	go func(in io.WriteCloser) {
-//		for i := 0; i < 5; i++ {
-//			in.Write([]byte(fmt.Sprintf("hoge_%d", i)))
-//			time.Sleep(1 * time.Second)
-//		}
-//	}(stdin)
-
-
-
-//	tests := []string{
-//		"hoge",
-//		"/bin/hoge",
-//		"./hoge",
-//		"/Users/shingoishimura/bin/appengine-java-sdk-1.8.9/bin/appcfg.sh",
-//		"appcfg.sh",
-//	}
-//
-//	for _, test := range (tests) {
-//		if _, err := exec.LookPath(test); err != nil {
-//			log.Print(err)
-//		}
-//	}
 }
